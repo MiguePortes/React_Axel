3abfbbc0f9d1d77d2278d9b644568c35d23ce4e1
MiguelPortes <miguelgompor167@gmail.com>
2025-08-07 12:09:04 -0600

    se agrego bienvenida

--- axel/src/App.jsx
+++ axel/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Bienvenida from "./components/Bienvenida";
+import Login from "./components/Login";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Bienvenida />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
